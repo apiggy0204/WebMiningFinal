91155ad3a7f43327c8125828afa49973994127c1
Chris Chao <apiggy0204@gmail.com>
2012-01-02 18:53:51 +0800

    update

--- src/crawler/crawl.py
+++ src/crawler/crawl.py
@@ -10,66 +10,67 @@ import re
 import codecs
 import os
 
+startId = 5122
+endId   = 7725
 
 siteUrl = "http://lyrics.oiktv.com/"
-lyricsdb_path = "../../resources/lyrics2/"
+lyricsDbPath = "../../resources/lyrics2/"
 
 def fetchSong(url):
+    try:
+        #Get song info from url
+        songInfo = {}
+        _get = url.split('?')[1]
+        tokens = _get.split('&')
+        for token in tokens:
+            toks = token.split('=')
+            songInfo[toks[0]] = int(toks[1])
+        #print songInfo
+        
+        #fetch the html
+        lyricsWeb = urllib2.urlopen(url)  
+        webContent = lyricsWeb.read()  
+        lyricsWeb.close()       
     
-    #Get song info from url
-    song_info = {}
-    _get = url.split('?')[1]
-    tokens = _get.split('&')
-    for token in tokens:
-        toks = token.split('=')
-        song_info[toks[0]] = int(toks[1])
-    #print song_info
+        soup = BeautifulSoup(webContent)
     
-    #fetch the html
-    lyricsWeb = urllib2.urlopen(url)  
-    webContent = lyricsWeb.read()  
-    lyricsWeb.close()       
-
-    soup = BeautifulSoup(webContent)
-
-    lyrics = soup.findAll(id="mylrc")[0].contents
-    author = soup.findAll(attrs={'class' : 'link_hb'})[0].contents
-    album = soup.findAll(attrs={'class' : 'link_hb'})[1].contents
-    title = soup.findAll(attrs={'class' : 'link_hb'})[2].contents
-
-    #write to an xml file    
-    xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" + "<xml>\n" + "<doc>\n"
-    xml += "<sid>" + str(song_info[u'sid']) + "</sid>\n"
-    xml += "<aid>" + str(song_info[u'aid']) + "</aid>\n"
-    xml += "<lid>" + str(song_info[u'lid']) + "</lid>\n"
-    xml += "<title>"
-    for w in title:
-        xml += w 
-    xml += "</title>\n"
-    xml += "<author>"
-    for w in author:
-        xml += w 
-    xml += "</author>\n"
-    xml += "<lyrics>\n"
-    for s in lyrics:
-        for w in s:
-            try:
+        lyrics = soup.findAll(id="mylrc")[0].contents
+        author = soup.findAll(attrs={'class' : 'link_hb'})[0].contents
+        album = soup.findAll(attrs={'class' : 'link_hb'})[1].contents
+        title = soup.findAll(attrs={'class' : 'link_hb'})[2].contents
+    
+        #write to an xml file    
+        xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" + "<xml>\n" + "<doc>\n"
+        xml += "<sid>" + str(songInfo[u'sid']) + "</sid>\n"
+        xml += "<aid>" + str(songInfo[u'aid']) + "</aid>\n"
+        xml += "<lid>" + str(songInfo[u'lid']) + "</lid>\n"
+        xml += "<title>"
+        for w in title:
+            xml += w 
+        xml += "</title>\n"
+        xml += "<author>"
+        for w in author:
+            xml += w 
+        xml += "</author>\n"
+        xml += "<lyrics>\n"
+        for s in lyrics:
+            for w in s:
                 xml += w
-            except TypeError:
-                pass
-    xml += "</lyrics>\n"
-    xml += "</doc>\n"
-    xml += "</xml>\n"
-    
-    #write into db    
-    filename = lyricsdb_path + str(song_info['lid']) + ".txt"
-    f = codecs.open(filename, 'w', 'utf-8')
-    f.write(xml)
-    f.close()
+        xml += "</lyrics>\n"
+        xml += "</doc>\n"
+        xml += "</xml>\n"
+        
+        #write into db    
+        filename = lyricsDbPath + str(songInfo['lid']) + ".txt"
+        f = codecs.open(filename, 'w', 'utf-8')
+        f.write(xml)
+        f.close()
+    except:
+        pass
 
 ########################start of main###################################
 
-for i in range(5030, 7725):
+for i in range(startId, endId):
     
     url = "http://lyrics.oiktv.com/singer.php?sid=" + str(i)
 
--- src/vocab/query_tf.py
+++ src/vocab/query_tf.py
@@ -16,27 +16,44 @@ tree = ET.parse(res_path + 'playlists2.xml')
 
 #f = codecs.open('query_tf.txt', 'w', 'utf-8')
 
+"""
+Retrieve titles of the songs for reference
+"""
+refSongTitles = []
 root = tree.getroot()
 for list in root:
     theme = list[0].text
     #print theme
     
-    if theme != None and re.search(query, theme, re.UNICODE) != None:
+    if theme != None and re.search(query, theme, re.UNICODE):
         print theme
         for song in list:
             title = song.text
-            if title != theme:
-                print '\t' + title
-                
+            if title != None and title != theme:
+                refSongTitles.append(title)
+                print '\t' + title                
+                                
+"""
+get refSong's id and lyrics
+"""
+               
 dbPath = res_path + 'lyrics2/' 
 fSongs = os.listdir(dbPath)
 for fSong in fSongs:
     try:
         xml = ET.parse(dbPath + fSong)
         root = xml.getroot()
-        authors = root[0].findall('author')
-        for author in authors:
-            print author.text
+        
+        lyrics = root[0].findall('lyrics')[0].text
+        author = root[0].findall('author')[0].text
+        title = root[0].findall('title')[0].text
+        sid = int(root[0].findall('sid')[0].text)
+        lid = int(root[0].findall('lid')[0].text)
+        
+        for refTitle in refSongTitles:
+            if title == refTitle:
+                print lyrics
+                break
         
     except:
         pass
